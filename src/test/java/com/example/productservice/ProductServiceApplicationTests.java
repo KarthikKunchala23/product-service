6169286eaacae8293f4646f5a0d424fdb1d5fd32
karthik <karthikkunchala2398@gmail.com>
2026-09-23 13:06:37 +0530

    unit test and integrate test separation and added step in ci

--- src/test/java/com/example/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/example/productservice/ProductServiceApplicationTests.java
@@ -2,9 +2,41 @@ package com.example.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class ProductServiceApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:18")
+                    .withDatabaseName("productdb")
+                    .withUsername("productuser")
+                    .withPassword("testpassword");
+
+    @DynamicPropertySource
+    static void configureDatabase(DynamicPropertyRegistry registry) {
+
+        registry.add(
+                "spring.datasource.url",
+                postgres::getJdbcUrl
+        );
+
+        registry.add(
+                "spring.datasource.username",
+                postgres::getUsername
+        );
+
+        registry.add(
+                "spring.datasource.password",
+                postgres::getPassword
+        );
+    }
 
     @Test
     void contextLoads() {
